2c5bb46e5ea0a3dfdf2c7587a60b654b85ae08d3
Aditya <adityabhattacharya608@gmail.com>
2025-09-03 02:05:32 +0530

    commit

--- supabase/migrations/20250808121000_5b0a5ed3-5395-4bd7-a791-76b84ecdf785.sql
+++
@@ -1,7 +0,0 @@
--- Update activity_logs to reference profiles table instead of auth.users
-ALTER TABLE public.activity_logs 
-DROP CONSTRAINT activity_logs_user_id_fkey;
-
-ALTER TABLE public.activity_logs 
-ADD CONSTRAINT activity_logs_user_id_fkey 
-FOREIGN KEY (user_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
